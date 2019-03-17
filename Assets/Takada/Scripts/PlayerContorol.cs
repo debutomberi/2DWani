74423b6965555fca2836f695af0b438df8aee5cc
高田将志 <masapo.0315@icloud.com>
2019-03-18 01:08:10 +0900

    ワニを上下左右斜めに移動できるようにした。

---
+++ Assets/Takada/Scripts/PlayerContorol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContorol : MonoBehaviour {
+
+    // 速度
+    float speed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // 移動処理
+        Move();
+
+	}
+
+    // 移動関数
+    void Move()
+    {
+        var inputX = Input.GetAxisRaw("Horizontal");
+        var inputY = Input.GetAxisRaw("Vertical");
+
+        this.transform.position += new Vector3(inputX * speed, inputY * speed, 0);
+    }
+}
