07181576f02955376b0f1b4d68252c13ce0b4885
natuto1998 <kouta.taka.0811.15@gmail.com>
2019-03-18 01:19:10 +0900

    魚の移動の幅と移動速度

--- Assets/MainScene/Scripts/Control/EnemyController.cs
+++ Assets/MainScene/Scripts/Control/EnemyController.cs
@@ -4,28 +4,48 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-    float speed = 1.0f;
-    float time;
+    int width;
 
-	void Start ()
+    float speed = 1.0f;
+    float time = 0;
+    bool switching = true;
+
+    Vector2 Scale;
+
+    void Start ()
     {
-        
+        width = Random.Range(2, 4);
+        //スケールを取得
+        Scale = transform.localScale;
     }
 	
 	void Update ()
-    {
+    {        
         time += Time.deltaTime;
         EnemyMove();
     }
     
     private void EnemyMove()
-    {
+    {        
         //魚の動き
-        transform.position += transform.right * speed;
-        if (time > 2)
+        transform.position += transform.right * speed;       
+        if (time > width)
         {
-            speed *= -1;
-            time = 0;
+            if(switching == true)
+            {
+                speed = Random.Range(-0.5f, -1f);
+                switching = false;
+            }
+            else
+            {
+                speed = Random.Range(0.5f, 1f);
+                switching = true;
+            }
+                      
+            width = Random.Range(2, 4);
+            Scale.x *= -1;
+            time = 0;            
         }
+        transform.localScale = Scale;
     }
 }
