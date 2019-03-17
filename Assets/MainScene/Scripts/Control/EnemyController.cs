1ec646cfa0e9bd70d868a88ae80f5457b8a00591
natuto1998 <kouta.taka.0811.15@gmail.com>
2019-03-18 00:17:25 +0900

    魚の本当に単純な動き

---
+++ Assets/MainScene/Scripts/Control/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    float speed = 1.0f;
+    float time;
+
+	void Start ()
+    {
+        
+    }
+	
+	void Update ()
+    {
+        time += Time.deltaTime;
+        EnemyMove();
+    }
+    
+    private void EnemyMove()
+    {
+        //魚の動き
+        transform.position += transform.right * speed;
+        if (time > 2)
+        {
+            speed *= -1;
+            time = 0;
+        }
+    }
+}
