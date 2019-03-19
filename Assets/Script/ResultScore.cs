10e7cb3a8ddcd8cf01f2c793dc8e879ea8bebafe
兎子-PC\兎子 <asahii.jr@gmail.com>
2019-03-19 10:51:58 +0900

    ScoreManagerの作成とtextへの反映

---
+++ Assets/Script/ResultScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultScore : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        scoreText.text = string.Format("{0:D5}", ScoreManager.Instance.Score);
+    }
+    
+}
---
+++ Assets/Script/ScoreText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    [SerializeField]
+    Text scoreText;
+
+    // Use this for initialization
+    void Start()
+    {
+        ScoreManager.Instance.ScoreReset();
+        scoreText.text = string.Format("{0:D5}", ScoreManager.Instance.Score);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = string.Format("{0:D5}", ScoreManager.Instance.Score);
+    }
+}
